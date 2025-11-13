003e5ae98b43e27389eed0167c6498454b842a91
samming0612 <sammi3915@gmail.com>
2025-11-13 15:48:56 +0800

    Updated dataset file path

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,7 +4,7 @@ from transformers import AutoModelForSeq2SeqLM, T5Tokenizer
 model = AutoModelForSeq2SeqLM.from_pretrained("aws/package/model")
 tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
 
-with open("../data_gen/training/eval_prompts.jsonl") as f:
+with open("data_gen/training/eval_prompts.jsonl") as f:
     for line in f:
         prompt = json.loads(line)["input"]
         inputs = tokenizer(prompt, return_tensors="pt")
